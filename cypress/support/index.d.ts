cc8ff1a50fa7b768d413af15ba732d2aa6e14ec6
Christoph Kruppe <c.kruppe@nfq.de>
2024-08-16 15:51:49 +0200

    Initial commit

---
+++ cypress/support/index.d.ts
@@ -0,0 +1,48 @@
+/* eslint-disable no-undef */
+/// <reference types="cypress" />
+
+import './commands';
+
+declare global {
+    namespace Cypress {
+        interface Chainable {
+            /**
+             * Custom command to select DOM element by data-cy attribute.
+             *
+             * @example cy.getCy('greeting')
+             */
+            getCy(value: string): Chainable<JQuery>;
+
+            /**
+             * Custom command to mount hooks for testing.
+             *
+             * @example cy.mountHook(useHook);
+             */
+            mountHook<T extends (...args: any) => any>(hook: T): {component: React.FC; values: ReturnType<T>};
+        }
+
+        interface Chainer<Subject> {
+            /**
+             * Custom Chai assertion that checks if given subject is an specific HTML element.
+             *
+             * @example
+             * ```
+             * expect('foo').to.be.an.htmlElement('button)
+             * cy.wrap('foo').should('be.an.htmlElement', 'button')
+             * ```
+             */
+            (chainer: 'be.an.htmlElement', value: string): Chainable<Subject>;
+            /**
+             * Custom Chai assertion that checks if given subject is an specific HTML element.
+             *
+             * @example
+             * ```
+             * expect('foo').to.not.be.an.htmlElement('button)
+             * cy.wrap('foo').should('not.be.an.htmlElement', 'button')
+             * ```
+             */
+            // eslint-disable-next-line @typescript-eslint/unified-signatures
+            (chainer: 'not.be.an.htmlElement', value: string): Chainable<Subject>;
+        }
+    }
+}
---
+++ rollup.config.mjs
@@ -0,0 +1,49 @@
+/* eslint-disable array-func/prefer-array-from */
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import cleaner from 'rollup-plugin-cleaner';
+
+// eslint-disable-next-line import/extensions
+import pkg from './package.json' assert { type: "json" };
+
+const globals = {};
+
+export default [
+    {
+        external: [...Object.keys({
+            ...pkg.devDependencies,
+            ...pkg.peerDependencies
+        } || {})],
+        input: 'src/index.tsx',
+        output: [
+            {
+                exports: 'named',
+                file: pkg.exports['.'].require,
+                format: 'cjs',
+                globals,
+                interop: 'auto',
+                name: pkg.name,
+                sourcemap: true
+            },
+            {
+                dir: './dist/esm/',
+                exports: 'named',
+                format: 'es',
+                globals,
+                name: pkg.name,
+                preserveModules: true,
+                sourcemap: true
+            }
+        ],
+        plugins: [
+            cleaner({targets: ['./dist/']}),
+            resolve({extensions: ['.js', '.jsx', '.json', '.ts', '.tsx']}),
+            commonjs({include: ['node_modules/**']}),
+            babel({
+                babelHelpers: 'bundled',
+                extensions: ['.js', '.jsx', '.json', '.ts', '.tsx']
+            })
+        ]
+    }
+];
